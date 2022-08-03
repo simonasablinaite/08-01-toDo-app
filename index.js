64ff792c0567e04dd6783d068c19122e1586369a
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-08-03 07:37:41 +0300

    Imported handlebars templates

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const port = 8081;
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+   extname: 'hbs'
+}));
+
+app.get('/test', (req, res) => res.render('test'));
 
 app.get('/', (req, res) => res.send('Labas'));
 
