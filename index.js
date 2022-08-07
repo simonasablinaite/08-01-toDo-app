92746ad6f339e385f020bea975f2cbdf981f4c5a
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-08-07 19:11:38 +0300

    errors, add, delete (style icons are missing)

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import mysql from 'mysql2';
-import mainPage from './lib/mainPage.js';
+import {renderMainPage, insertNewNote, deleteNote} from './lib/mainPage.js';
 
 const app = express();
 const port = 8081;
 app.use(express.static('public'));
+app.use(express.urlencoded({
+   extended: true
+}));
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
    extname: 'hbs'
 }));
 
-app.get('/', mainPage);
-
-//DB start
-const connection = mysql.createConnection({
-   host: 'localhost',
-   database: 'classicmodels',
-   user: 'classicmodels',
-   password: 'bit'
-});
-
-app.get('/db', (req, res) => {
-   connection.execute('SELECT productLine, textDescription FROM productlines', (err, rows) => {
-      const data = rows.map(row => row);
-      res.render('db', {data: data})
-   })
-});
+app.get('/', renderMainPage);
+app.post('/', insertNewNote);
+app.delete('/', deleteNote);
 
 app.listen(port, () => console.log(`Starting server on port ${port}`));
